d2f88b5796760ee2fb2f261f0affe0192408de55
Surendra Kumar B <vvsurendra.boddu@gmail.com>
2018-02-13 22:42:29 +0530

    Added few solutions in 30DaysOfCode module.

---
+++ 30DaysOfCode/DayOneDataTypes.js
@@ -0,0 +1,4 @@
+/**
+ * Problem : https://www.hackerrank.com/challenges/30-data-types/problem
+ *
+ */
---
+++ 30DaysOfCode/DayZeroHelloWorld.js
@@ -0,0 +1,13 @@
+/**
+ * Problem : https://www.hackerrank.com/challenges/30-hello-world/problem
+ *
+ * @param {string} inputString
+ */
+
+function processData(inputString) {
+  // This line of code prints the first line of output
+  console.log("Hello, World.");
+
+  // Write the second line of output that prints the contents of 'inputString' here.
+  console.log(inputString);
+}
